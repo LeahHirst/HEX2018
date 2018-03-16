16c2d21afd4c3de086f33d39fb83781ddb051efa
Gavin Henderson <gavin.henderson@hotmail.co.uk>
2018-03-17 00:57:37 +0100

    Added DB connection

--- server/index.js
+++ server/index.js
@@ -1,4 +1,7 @@
 const express   = require('express');
 const app       = express();
+const mongoose  = require('mongoose')
+
+let db = require('./app/database.js')(mongoose);
 
 app.listen(3000, () => console.log("Debugging on port 3000"));
