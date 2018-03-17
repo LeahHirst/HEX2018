d7eb880db81ea6c701cf4a51062981df42046d81
Gavin Henderson <gavin.henderson@hotmail.co.uk>
2018-03-17 01:22:37 +0100

    Changed secret from 'vathack' to 'hexhack'

--- server/index.js
+++ server/index.js
@@ -32,7 +32,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(session({
-	secret: 'hackvat',
+	secret: 'hexhack',
 	resave: false,
 	saveUninitialized: false
 }));
