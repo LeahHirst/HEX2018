011b9b39696bc658d4e02da45350fd6b92c85513
Conor Haining <conor.haining@gmail.com>
2018-03-17 09:32:40 +0000

    Adding Twilio Credentials

--- server/app/twilio.js
+++ server/app/twilio.js
@@ -1,3 +1,9 @@
 module.exports = (db) => {
-  
+  // Test Credentials
+  const accountSID = "AC4cf113b590211dbd3f7412cf98839446";
+  const authToken = "6a15dcb9d4287aca8b6da25d381d2aa2";
+
+  // Live Credentials
+  //const accountSID = "ACdcc80bff21eb2531213430a9bfeeda0c";
+  //const authToken = "8aa1635b0eabcd93cac79970e3d07706";
 }
