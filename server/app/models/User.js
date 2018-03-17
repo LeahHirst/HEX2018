76c2e308ca59efdf61d39ee3374d51b8f21e334b
Gavin Henderson <gavin.henderson@hotmail.co.uk>
2018-03-17 08:44:25 +0100

    Added User model

---
+++ server/app/models/User.js
@@ -0,0 +1,19 @@
+module.exports = new require('mongoose').Schema({
+  password: {
+    type: String,
+    require: true
+  },
+  email: {
+    type: String,
+    required: true
+  },
+  name: {
+    type: String,
+    required: true
+  },
+  type: {
+    type: "String",
+    enum: ['Craftor','Patron'],
+    required: true
+  }
+});
--- server/app/models/models.js
+++ server/app/models/models.js
@@ -1,5 +1,5 @@
 const mongoose = require("mongoose");
 
 module.exports = {
-	//User: mongoose.model("User", require("./User.js")),
+	User: mongoose.model("User", require("./User.js")),
 };
