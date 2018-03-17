18ef107adfa320e5ccc593052d8be427ffffaa34
Gavin Henderson <gavin.henderson@hotmail.co.uk>
2018-03-17 01:11:46 +0100

    Server now uses passport

---
+++ server/app/routes.js
@@ -0,0 +1,5 @@
+module.exports = (app, passport, db) => {
+  app.get('/', (req,res)=>{
+    res.send("Hello World")
+  })
+}
--- server/index.js
+++ server/index.js
@@ -1,7 +1,51 @@
-const express   = require('express');
-const app       = express();
-const mongoose  = require('mongoose')
+const express       = require('express');
+const app           = express();
+const mongoose      = require('mongoose');
+const passport      = require('passport');
+const bodyParser    = require('body-parser');
+const cookieParser  = require('cookie-parser');
+const session       = require('express-session');
+const flash         = require('connect-flash');
+const LocalStrategy = require('passport-local').Strategy;
 
 let db = require('./app/database.js')(mongoose);
+
+passport.serializeUser(function(user, done) {
+	done(null, user._id);
+});
+
+passport.deserializeUser(function(id, done) {
+	db.model.User.findOne({ _id: id }, (err, user) => {
+		if (err) return done(err);
+		if (!user) return done(null, false, 'User not found.');
+		return done(null, user);
+	});
+});
+
+// Setup view engine
+app.set('view engine', 'ejs');
+
+// Setup a static folder
+app.use(express.static('public'));
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(session({
+	secret: 'hackvat',
+	resave: false,
+	saveUninitialized: false
+}));
+app.use(passport.initialize());
+app.use(passport.session());
+app.use(flash());
+
+passport.use(new LocalStrategy({
+  usernameField: "email",
+	passwordField: "password"
+}, require('./app/authenticate.js')(db)));
+
+// Load routes
+require('./app/routes.js')(app, passport, db);
 
 app.listen(3000, () => console.log("Debugging on port 3000"));
